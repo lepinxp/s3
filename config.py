bfad2e943cf992fd9360fcd145be69cb23319057
BenyXP <103335260+BenyXP@users.noreply.github.com>
2022-07-05 21:09:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -26,8 +26,6 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "0"))
 FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "0"))
-FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "0"))
-
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
